4e0861384488969e4fcb9216b6ccae9cc87fa510
XR_XharpRazor <114336132+XR-XharpRazor@users.noreply.github.com>
2026-07-05 20:54:49 +0800

    a lot
    - now words from dictionary depends on difficulty
    - words are generated based on 2 params : word length and word amount
    - words with shorter length drops faster
    - now added commands "/play", "/pause", "/pwrt", "/pwrs"

--- main.js
+++ main.js
@@ -38,6 +38,12 @@ function POwO_Math_IsInRange_Exclusive(inMin, inVal, inMax)
     return inMin < inVal && inVal < inMax
 }
 
+//---- ---- ---- ---- helping functions
+
+function POwO_JS_ArrayRandomPickOne(inArray)
+{
+    return inArray[ Math.floor(Math.random() * inArray.length) ]
+}
 
 
 
@@ -67,7 +73,7 @@ class TextNOwOde
 
     updateW()
     {
-        this.width = this.widthMargin * 2 + this.text.length * this.fontToWidthRatio
+        this.width = this.widthMargin * 2 + (this.text.length + this.effect.length) * this.fontToWidthRatio
     }
 
     move ()
@@ -90,7 +96,7 @@ class TextNOwOde
         ctx.textAlign = "center"
         ctx.textBaseline = "middle"
         ctx.fillStyle = this.colorText
-        ctx.fillText(this.text, this.posX, this.posY)
+        ctx.fillText(this.text + this.effect, this.posX, this.posY)
     }
 }
 
@@ -124,24 +130,26 @@ const HTML_Body = POwO_docgetel("HTML_body")
 
 var GLOBAL_PromptArray = []
 var GLOBAL_GroundLine = 900
-var GLOBAL_UserTextNode = new TextNOwOde("", ctx.canvas.width / 2 , GLOBAL_GroundLine + 100, 0, 25, 100, "72px Calibri", 29, "rgba(255,192,0,0.25)","rgba(255,192,0,1)")
+var GLOBAL_UserTextNode = new TextNOwOde("", ctx.canvas.width / 2 , GLOBAL_GroundLine + 100, 0, 25, 100, "72px Calibri", 29, "rgba(255,192,0,0.25)","rgba(255,192,0,1)","")
+var GLOBAL_canvasWidthMargin = 200
 
+var GLOBAL_gameState = "pause"
 var GLOBAL_Diff = 0 //0~15, each 1min30sec
 var GLOBAL_Score = 0
 var GLOBAL_Goal = 100
 var GLOBAL_HP = 50 //each character is 1HP
-var GLOBAL_Hourglass = new hOwOrglass(500, 1500, 0)
+var GLOBAL_Hourglass = new hOwOrglass(750, 1000, 0)
 
 var GLOBAL_Pocket_TimeAdd = 0
 var GLOBAL_Pocket_NodeSlow = 0
 
 
 const GLOBAL_Dictionary_Response = await fetch("./Dictionary-main.txt")
-const GLOBAL_Dictionaey_WholeString = (await GLOBAL_Dictionary_Response.text()).split(/\r?\n/)
+const GLOBAL_Dictionary_WholeString = (await GLOBAL_Dictionary_Response.text()).split(/\r?\n/)
 var GLOBAL_Dictionary_Main = [ [],[],[],[], [],[],[],[], [],[],[],[], [],[],[],[] ]
-for(let i = 0 ; i < GLOBAL_Dictionaey_WholeString.length ; i++)
+for(let i = 0 ; i < GLOBAL_Dictionary_WholeString.length ; i++)
 {
-    let temp_currentString = GLOBAL_Dictionaey_WholeString[i] 
+    let temp_currentString = GLOBAL_Dictionary_WholeString[i] 
     let temp_currentStringLength = temp_currentString.length
 
     if (0 < temp_currentStringLength && temp_currentStringLength < 16)
@@ -154,9 +162,6 @@ for(let i = 0 ; i < GLOBAL_Dictionaey_WholeString.length ; i++)
     }
 }
 console.log(GLOBAL_Dictionary_Main)
-
-
-var GLOBAL_Dictionary = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","t","s","u","v","w","x","y","z"]
 
 /*
 
@@ -179,22 +184,34 @@ level 7 : memes
 function POwO_Dictionary_GeneratePrompt()
 {
     //base pm the current difficulty, which word list should we choose from the dictionary ?
+    //wordLengthDistribution : how long the world should be ? = l
+    //wordQuantityDistribution : how many words for this wave ? = q
     let temp_Table_fromDiffToIndex = 
     [
-        [0,1,1,2,2,2],
-        [2,2,3,3,3,4,4],
-        [4,4,5,5,5,6,6],
-        [4,5,5,5,6,6,6,7,8],
+        {l : [0,1,1,2,2,2],                                 q : [3,4,5,6]},
+        {l : [2,2,3,3,3,4,4],                               q : [3,4,5,5]},
+        {l : [4,4,5,5,5,6,6],                               q : [3,4,4,5]},
+        {l : [4,5,5,5,6,6,6,7,8],                           q : [3,4,5]},
 
-        [5,5,6,6,6,7,7,7,8,9,10],
-        [5,5,6,6,6,7,7,7,8,8,9,10,11,12],
-        [5,5,6,6,6,7,7,7,8,8,8,9,9,10,11,12,13,14],
-        [15]
+        {l : [5,5,6,6,6,7,7,7,8,9,10],                      q : [2,3,4]},
+        {l : [5,5,6,6,6,7,7,7,8,8,9,10,11,12],              q : [2,3,3,4] },
+        {l : [5,5,6,6,6,7,7,7,8,8,8,9,9,10,11,12,13,14],    q : [1,2,2,3,3]},
+        {l : [15],                                          q : [1]}
     ]
 
-    let temp_GrabIndex = temp_Table_fromDiffToIndex[ GLOBAL_Diff ][ Math.floor(Math.random() * temp_Table_fromDiffToIndex[GLOBAL_Diff].length) ]
+    let temp_currentSelectedObject = temp_Table_fromDiffToIndex[ GLOBAL_Diff ]
+    let temp_selected_diffIndex = POwO_JS_ArrayRandomPickOne(temp_currentSelectedObject.l)
+    let temp_selected_wordQuantity = POwO_JS_ArrayRandomPickOne(temp_currentSelectedObject.q)
 
-    return GLOBAL_Dictionary_Main[ temp_GrabIndex ][ Math.floor(Math.random() * GLOBAL_Dictionary_Main[ temp_GrabIndex ].length) ]
+    let temp_returnString = ""
+
+    for(let i = 0 ; i < temp_selected_wordQuantity ; i++)
+    {
+        temp_returnString += POwO_JS_ArrayRandomPickOne(GLOBAL_Dictionary_Main[ temp_selected_diffIndex ])
+        if (i < temp_selected_wordQuantity - 1){temp_returnString += " "}
+    }
+
+    return temp_returnString
 }
 
 
@@ -209,10 +226,18 @@ function POwO_RedrawAll()
     ctx.textAlign = "left"
     ctx.textBaseline = "middle"
     ctx.fillStyle = "#ffffff"
-    ctx.fillText("🚀 : " + GLOBAL_Score.toString() + "/" + GLOBAL_Goal.toString(), temp_StatusTag_X, temp_StatusTag_Y * 1)
+    ctx.fillText("🚀 : " + GLOBAL_Score.toString() + "/" + GLOBAL_Goal.toString() + " , " + GLOBAL_Diff.toString() + "/15", temp_StatusTag_X, temp_StatusTag_Y * 1)
     ctx.fillText("❤ : " + GLOBAL_HP.toString(), temp_StatusTag_X, temp_StatusTag_Y * 2)
     ctx.fillText("⏳ : " + GLOBAL_Pocket_TimeAdd.toString(), temp_StatusTag_X, temp_StatusTag_Y * 3)
     ctx.fillText("🐌 : " + GLOBAL_Pocket_NodeSlow.toString(), temp_StatusTag_X, temp_StatusTag_Y * 4)
+
+    if (GLOBAL_gameState === "pause")
+    {
+        ctx.font = "100px Calibri"
+        ctx.textAlign = "center"
+        ctx.fillStyle = "rgba(255,255,255,0.25)"
+        ctx.fillText("PAUSED" , canvas.width / 2 , canvas.height / 2)
+    }
 
     //draw all prompts
     for(let i = 0 ; i < GLOBAL_PromptArray.length ; i++)
@@ -235,6 +260,93 @@ function POwO_RedrawAll()
 
 
 
+
+
+
+// ---- ---- ---- ---- RUN MAIN
+
+POwO_RedrawAll()
+
+function POwO_Interval_Tick()
+{
+    //drop all prompts
+    for(let i = 0 ; i < GLOBAL_PromptArray.length ; i++)
+    {
+        GLOBAL_PromptArray[i].move()
+
+        if (GLOBAL_PromptArray[i].posY > GLOBAL_GroundLine)
+        {
+            GLOBAL_HP -= GLOBAL_PromptArray[i].text.length
+            GLOBAL_PromptArray.splice(i,1)
+            
+            if (GLOBAL_HP < 0)
+            {
+                console.log("player lose")
+                clearInterval(GLOBAL_Interval_Run)
+            }
+        }
+    }
+
+    //drain hourglass
+    let FLAG_makeNewWords = GLOBAL_Hourglass.drain()
+    if ( FLAG_makeNewWords )
+    {
+        //generate new words
+
+        let temp_String_Array = POwO_Dictionary_GeneratePrompt().split(" ")
+
+        for(let i = 0 ; i < temp_String_Array.length ; i++)
+        {
+            let temp_makeNode_text = temp_String_Array[i];
+
+            let temp_makeNode_PosX = -1;
+            if (temp_String_Array.length > 1)
+            {
+                temp_makeNode_PosX = POWO_Math_Lerp(GLOBAL_canvasWidthMargin, ctx.canvas.width - GLOBAL_canvasWidthMargin, i / (temp_String_Array.length - 1))
+            }
+            else
+            {
+                temp_makeNode_PosX = POWO_Math_Lerp(GLOBAL_canvasWidthMargin, ctx.canvas.width - GLOBAL_canvasWidthMargin, Math.random() )
+            }
+
+            let temp_makeNode_vel = 0;
+            if (1 <= temp_makeNode_text.length && temp_makeNode_text.length <= 4){temp_makeNode_vel = 1;}
+            else if (5 <= temp_makeNode_text.length && temp_makeNode_text.length <= 10){temp_makeNode_vel = 0.5;}
+            else if (11 <= temp_makeNode_text.length){temp_makeNode_vel = 0.25;}
+
+            let temp_makeNode_effect_pool = ["","","","","","","","","","❤","⏳","🐌"] //❤ = HP add, ⏳ = time add duration, 🐌 = slow nodes
+            let temp_makeNode_effect = temp_makeNode_effect_pool[ Math.floor( Math.random() * temp_makeNode_effect_pool.length ) ]
+            
+            let temp_makeNode_fillColor = "rgba(32,32,32,0.5)"
+            let temp_makeNode_textColor = "rgba(255,255,255,1)"
+            if (temp_makeNode_effect.length > 1)
+            {
+                temp_makeNode_fillColor = "rgba(0,255,128,0.5)"
+                temp_makeNode_textColor = "rgba(0,192,64,1)"
+            }
+
+            GLOBAL_PromptArray.push
+            (
+                new TextNOwOde
+                (
+                    temp_makeNode_text, //inText
+                    temp_makeNode_PosX, //inPosX
+                    0 , //inPosY
+                    temp_makeNode_vel, //inVel
+                    50, //widthMargin
+                    80, //height
+                    "50px Calibri", //inFont
+                    12, //inFontToWidthRatio
+                    temp_makeNode_fillColor, //inFillColor
+                    temp_makeNode_textColor, //inTextColor
+                    temp_makeNode_effect
+                )
+            )
+        }
+    }
+    POwO_RedrawAll()
+}
+
 window.addEventListener("message",(event) => {
     
 })
@@ -246,27 +358,57 @@ window.addEventListener("keydown",(event) => {
     {
         let temp_userText = GLOBAL_UserTextNode.text
 
-        for(let i = GLOBAL_PromptArray.length - 1 ; 0 <= i ; i--)
+        if (temp_userText.at(0) === '/')
         {
-            if (GLOBAL_PromptArray[i].text === temp_userText)
+            //it is a special command
+            switch (temp_userText)
             {
-                let temp_currentNode = GLOBAL_PromptArray[i]
-
-                //add score
-                GLOBAL_Score += temp_userText.length
-
-                //effects ?
-                switch (temp_currentNode.effect) {
-                    case "❤" : GLOBAL_HP += temp_currentNode.text.length ; break ;
-                    case "⏳" : GLOBAL_Pocket_TimeAdd ++ ; break;
-                    case "🐌" : GLOBAL_Pocket_NodeSlow ++ ; break ;
-                    default : break ;
-                }
-
-                //delete node
-                GLOBAL_PromptArray.splice(i,1)
+                case "/play" : GLOBAL_Interval_Run = setInterval(()=>{ POwO_Interval_Tick() },1) ; GLOBAL_gameState = "play" ; break ;
+                case "/pause" : clearInterval(GLOBAL_Interval_Run) ; GLOBAL_gameState = "pause" ; POwO_RedrawAll() ; break ;
+                case "/pwrt" : if (GLOBAL_Pocket_TimeAdd > 0){ GLOBAL_Hourglass.cur += 1000 ; GLOBAL_Pocket_TimeAdd -- } ; break ;
+                case "/pwrs" : if (GLOBAL_Pocket_NodeSlow > 0){ for(let i = 0 ; i < GLOBAL_PromptArray.length ; i++){ GLOBAL_PromptArray[i].vel /= 2 } ; GLOBAL_Pocket_NodeSlow -- } ; break ;
+            
+                default: break ;
             }
         }
+        else if (GLOBAL_gameState === "play")
+        {
+            //just normal typing
+            for(let i = GLOBAL_PromptArray.length - 1 ; 0 <= i ; i--)
+            {
+                if (GLOBAL_PromptArray[i].text === temp_userText)
+                {
+                    let temp_currentNode = GLOBAL_PromptArray[i]
+
+                    //add score
+                    GLOBAL_Score += temp_userText.length
+                    while (GLOBAL_Score > GLOBAL_Goal)
+                    {
+                        GLOBAL_Score -= GLOBAL_Goal
+                        GLOBAL_Diff++
+                    }
+
+                    if (GLOBAL_Diff > 15)
+                    {
+                        console.log("player win")
+                        clearInterval(GLOBAL_Interval_Run)
+                    }
+
+                    //effects ?
+                    switch (temp_currentNode.effect) {
+                        case "❤" : GLOBAL_HP += temp_currentNode.text.length ; break ;
+                        case "⏳" : GLOBAL_Pocket_TimeAdd ++ ; break;
+                        case "🐌" : GLOBAL_Pocket_NodeSlow ++ ; break ;
+                        default : break ;
+                    }
+
+                    //delete node
+                    GLOBAL_PromptArray.splice(i,1)
+                }
+            }
+        }
+
+        
 
 
         GLOBAL_UserTextNode.text = ""
@@ -275,57 +417,19 @@ window.addEventListener("keydown",(event) => {
     {
         GLOBAL_UserTextNode.text = GLOBAL_UserTextNode.text.slice(0,-1)
     }
-    else if (event.key === "Shift" || event.key === "Control")
+    else if (event.key === "Shift" || event.key === "Control" || event.key === "Meta")
     {
         //ignore
     }
     else
     {
         GLOBAL_UserTextNode.text += event.key
+        
+        
     }
+
+    if (GLOBAL_gameState === "pause"){POwO_RedrawAll()}
 })
 
-
-// ---- ---- ---- ---- RUN MAIN
-
-POwO_RedrawAll()
-
-setInterval(()=>{
-
-    //drop all prompts
-    for(let i = 0 ; i < GLOBAL_PromptArray.length ; i++)
-    {
-        GLOBAL_PromptArray[i].move()
-
-        if (GLOBAL_PromptArray[i].posY > GLOBAL_GroundLine)
-        {
-            GLOBAL_PromptArray.splice(i,1)
-        }
-    }
-
-    //drain hourglass
-    let FLAG_makeNewWords = GLOBAL_Hourglass.drain()
-    if ( FLAG_makeNewWords )
-    {
-        //generate new words
-        GLOBAL_PromptArray.push
-        (
-            new TextNOwOde
-            (
-                POwO_Dictionary_GeneratePrompt(),
-                Math.random() * ctx.canvas.width,
-                0,
-                Math.random(), //drop velocity
-                50,
-                80,
-                "50px Calibri",
-                12,
-                "rgba(32,32,32,0.5)",
-                "rgba(255,255,255,1)"
-            )
-        )
-    }
-
-    POwO_RedrawAll()
-},1)
+var GLOBAL_Interval_Run = 0
 
